a1bf18adac5866085ebf14812b91f48276e244c8
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-04 20:20:37 +0330

    db: config typeorm forroot

--- e-learning[api]/src/app.module.ts
+++ e-learning[api]/src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import config from 'config';
 
 @Module({
   imports: [
-    AuthModule,
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'auth',
+      entities = [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
