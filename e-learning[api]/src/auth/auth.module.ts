0da9aac938f071938c3e2b03f99974a7d950f936
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-24 13:30:22 +0330

    Adding [jwt]-passport strategy to auth.module.

--- e-learning[api]/src/auth/auth.module.ts
+++ e-learning[api]/src/auth/auth.module.ts
@@ -2,11 +2,20 @@ import { AuthController } from './auth.controller';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { User } from './entities/user.entity';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtStategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'super secret',
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService, JwtStategy],
 })
 export class AuthModule {}
