83aef254b16b89f16eeb3f4876b3c82ca8a30c08
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-24 09:50:00 +0330

    validate user utilities in auth.service.

--- e-learning[api]/src/auth/auth.service.ts
+++ e-learning[api]/src/auth/auth.service.ts
@@ -17,4 +17,31 @@ export class AuthService {
   logOut() {
     return 'user logged out';
   }
+  async validateUser(username: string, password: string) {
+    const user = await this.getUser(username);
+    if (user) {
+      if (this.matchPassword(password, user.password)) {
+        return user;
+      }
+    }
+    return new HttpException(
+      'wrong username, or password',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+  createPassword(password: string) {
+    const salt = randomBytes(16).toString();
+    return this.encreptPassword(password, salt);
+  }
+  encreptPassword(password: string, salt: string) {
+    const hashed = scryptSync(password, salt, 64);
+    return `${salt}:${hashed}`;
+  }
+  matchPassword(password: string, expectpassword: string) {
+    const [salt, _] = expectpassword.split(':');
+    const encpass = this.encreptPassword(password, salt);
+    const hashedpass = Buffer.from(encpass);
+    const expectedpass = Buffer.from(expectpassword);
+    return timingSafeEqual(hashedpass, expectedpass);
+  }
 }
