c2634499fa4887ebc2bd140360ca64492a931768
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-24 13:30:22 +0330

    Adding [jwt]-passport strategy to auth.module.

---
+++ e-learning[api]/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,5 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+export class JwtStategy extends PassportStrategy(Strategy) {
+}
