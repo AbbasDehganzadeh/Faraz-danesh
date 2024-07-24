d995667cdde982f37b581b8ce1b3eebcba524234
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-24 09:55:34 +0330

    update login & signup routes in auth.controller.ts

--- e-learning[api]/src/auth/auth.controller.ts
+++ e-learning[api]/src/auth/auth.controller.ts
@@ -1,27 +1,29 @@
 import { AuthService } from './auth.service';
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { LoginUserDto, SignupUserDto } from './dtos/user.dto';
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private authservice: AuthService) {}
+  constructor(private authService: AuthService) {}
   @Get('me')
   getUser() {
-    return this.authservice.getUser();
+    return this.authService.getUserById(1); //TODO: dynamic ID
   }
-  @Post('register')
-  register() {
-    return this.authservice.register();
+  @Post('signup')
+  signup(@Body() body: SignupUserDto) {
+    return this.authService.signup(body);
   }
   @Post('login')
-  logIn() {
-    return this.authservice.logIn();
+  logIn(@Body() body: LoginUserDto) {
+    // const [username, password] = body;
+    return this.authService.logIn(body);
   }
   @Post('refresh')
   refresh() {
-    return this.authservice.refreshToken();
+    return this.authService.refreshToken();
   }
   @Delete('logout')
   logOut() {
-    return this.authservice.logOut();
+    return this.authService.logOut();
   }
 }
