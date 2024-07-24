2a694619ef3897bacfa0b66d53a5510e1ca7b2a5
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-24 22:02:49 +0330

    Resolving my faults[it should\'ve been done in dbstuff branch]

--- e-learning[api]/src/auth/entities/user.entity.ts
+++ e-learning[api]/src/auth/entities/user.entity.ts
@@ -1,29 +1,32 @@
-import { Column, Entity } from 'typeorm';
-const Role = {
-    "STUDENT",
-    "TEACHER",
-    "SUPERVISOR"
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+enum Role {
+  STUDENT = 0,
+  TEACHER,
+  SUPERVISOR,
 }
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Column({ length: 20, nullable: true })
   fname: string;
 
   @Column({ length: 20 })
   lname: string;
 
-  @Column({ length: 20, nullable: false })
+  @Column({ length: 20, nullable: false, unique: true })
   uname: string;
 
   @Column({ unique: true, nullable: false })
   email: string;
 
-  @Column({ unique: true, nullable: false })
+  @Column({ unique: true, nullable: true })
   phone: string;
 
   @Column({ nullable: false })
   password: string;
 
-  @Column({enum:Role})
-  role: String;
+  @Column({ enum: Role, default: Role.STUDENT })
+  role: Role;
 }
