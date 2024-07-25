d7e9e6eabd680a7c579bcfa2f0624d6752815a30
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-25 20:55:09 +0330

    adding boilerplate for content routes.

--- e-learning[api]/src/content/content.controller.ts
+++ e-learning[api]/src/content/content.controller.ts
@@ -1,8 +1,56 @@
-import { Controller } from "@nestjs/common";
-import { ContentService } from "./content.service";
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { ContentService } from './content.service';
 
-@Controller()
+@Controller('/api')
 export class ContentController {
-    constructor(private contentService:ContentService) {}
-    
+  constructor(private contentService: ContentService) {}
+  @Get('course')
+  getCourses() {
+    return this.contentService.findCourses();
+  }
+  @Get('course/:slug')
+  getCourse() {
+    return this.contentService.getCourse();
+  }
+  @Post('course')
+  newCourse() {
+    return this.contentService.createCourse();
+  }
+  @Put('course/:slug')
+  updateCourse() {
+    return this.contentService.updateCourse();
+  }
+  @Put('course/:slug/publish')
+  publishCourse() {
+    return this.contentService.updateCourse();
+  }
+  @Delete('course/:slug/archive')
+  archiveCourse() {
+    return this.contentService.archiveCourse();
+  }
+
+  @Get('tutorial')
+  getTutorials() {
+    return this.contentService.findTutorials();
+  }
+  @Get('tutorial/:slug')
+  getTutorial() {
+    return this.contentService.getTutorial();
+  }
+  @Post('tutorial')
+  newTutorial() {
+    return this.contentService.createTutorial();
+  }
+  @Put('tutorial/:slug')
+  updateTutorial() {
+    return this.contentService.updateTutorial();
+  }
+  @Put('tutorial/:slug/publish')
+  publishTutorial() {
+    return this.contentService.publishTutorial();
+  }
+  @Delete('tutorial/:slug/archive')
+  archiveTutorial() {
+    return this.contentService.archiveTutorial();
+  }
 }
--- e-learning[api]/src/content/content.service.ts
+++ e-learning[api]/src/content/content.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CourseDocument } from './schma/course.schma';
+import { TutorialDocument } from './schma/tutorial.schma';
 import { Model } from 'mongoose';
 
 @Injectable()
@@ -9,4 +10,45 @@ export class ContentService {
     @InjectModel('courses') private courseModel: Model<CourseDocument>,
     @InjectModel('tutorials') private TutorialModel: Model<TutorialDocument>,
   ) {}
+  findCourses() {
+    return 'find courses';
+  }
+  getCourse() {
+    return 'get course';
+  }
+  createCourse() {
+    return 'create course';
+  }
+  updateCourse() {
+    return 'update course';
+  }
+  //TODO makePublish [generic function]
+  publishCourse() {
+    return 'publish course';
+  }
+  //TODO makeArchive [generic function]
+  archiveCourse() {
+    return 'archive course';
+  }
+
+  findTutorials() {
+    return 'find tutorials';
+  }
+  getTutorial() {
+    return 'get tutorial';
+  }
+  createTutorial() {
+    return 'create tutorial';
+  }
+  updateTutorial() {
+    return 'update tutorial';
+  }
+  //TODO makePublish [generic function]
+  publishTutorial() {
+    return 'publish tutorial';
+  }
+  //TODO makeArchive [generic function]
+  archiveTutorial() {
+    return 'archive tutorial';
+  }
 }
