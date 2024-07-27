5b0f5bad04aa105585ffdf2f08751346fc1d4d1b
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-27 22:57:02 +0330

    Update course schema:version: string\tversions: string[]\tdraft: boolean\tprice: number\ttags: string[]\trate: number\tcomments: Comment interface.

--- e-learning[api]/src/content/schma/tutorial.schma.ts
+++ e-learning[api]/src/content/schma/tutorial.schma.ts
@@ -1,5 +1,7 @@
 import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { Comment } from './comments.schema';
+import { Section } from './section.schema';
 
 export type TutorialDocument = HydratedDocument<Tutorial>;
 
@@ -9,13 +11,34 @@ class Tutorial {
   slug: string;
 
   @Prop()
+  version?: string;
+
+  @Prop()
+  versions: string[];
+
+  @Prop()
   name: string;
 
   @Prop()
   description: string;
 
   @Prop()
+  draft: boolean;
+
+  @Prop()
   teachers: number[];
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  tags: string[];
+
+  @Prop({ type: Types.Decimal128 })
+  rate: number;
+
+  @Prop()
+  comments: Comment[];
 }
 
 export const TutorialSchema = SchemaFactory.createForClass(Tutorial);
