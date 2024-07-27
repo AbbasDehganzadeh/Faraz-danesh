33e27c53af86b8a6bf6fd1d8f544fb56b9107733
abbas80 <dehghanzadehabbas@gmail.com>
2024-07-27 16:03:05 +0330

    Returning proper response with Dtos in -Get /me routes.

--- e-learning[api]/src/auth/auth.controller.ts
+++ e-learning[api]/src/auth/auth.controller.ts
@@ -1,15 +1,26 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, Delete, Get, Post, UseGuards } from '@nestjs/common';
-import { LoginUserDto, SignupUserDto } from './dtos/user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { LoginUserDto, ResponseUserDto, SignupUserDto } from './dtos/user.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getUser() {
-    return this.authService.getUserById(1); //TODO: dynamic ID
+  getUser(@Req() req: any): Promise<ResponseUserDto> {
+    const { user } = req;
+    console.info({ user });
+    return this.authService.getMe(user); //TODO: dynamic ID base on jwt token
   }
   @Post('signup')
   signup(@Body() body: SignupUserDto) {
