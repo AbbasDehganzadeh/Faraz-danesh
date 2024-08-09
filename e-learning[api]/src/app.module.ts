405a47c376cd642cf018686f736db940e7b0656e
abbas80 <dehghanzadehabbas@gmail.com>
2024-08-09 12:54:59 +0330

    Add redis database to project

--- e-learning[api]/src/app.module.ts
+++ e-learning[api]/src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import config from 'config';
 import { User } from './auth/entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from './redisdb/redis.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/elearning'),
     AuthModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
