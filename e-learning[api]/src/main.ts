d604e1b61e55b6b9669051d5c21922afbaf2ba9d
abbas80 <dehghanzadehabbas@gmail.com>
2024-08-10 16:32:43 +0330

    Set `whitelist` to true for dropping non-dto bodies.

--- e-learning[api]/src/main.ts
+++ e-learning[api]/src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // app.use(morgan)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3024);
 }
 bootstrap();
