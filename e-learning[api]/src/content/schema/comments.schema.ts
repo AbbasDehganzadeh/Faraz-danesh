93de48816a1aa6074ceabd0f829d7acb9e83901a
abbas80 <dehghanzadehabbas@gmail.com>
2025-04-24 10:20:02 +0330

    create an schema for 'Comments'

--- e-learning[api]/src/content/schema/comments.schema.ts
+++ e-learning[api]/src/content/schema/comments.schema.ts
@@ -1,4 +1,10 @@
-export interface Comment {
+import { Prop, Schema } from "@nestjs/mongoose";
+
+@Schema({ _id: false , timestamps: true})
+export class Comment {
+  @Prop({length: 250})
   text: string;
+
+  @Prop({isInteger: true})
   rate: number;
 }
