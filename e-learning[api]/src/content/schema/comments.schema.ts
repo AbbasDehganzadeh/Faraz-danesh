caef6f79ab7f2f290cad32589e8fa8f8b5b2ef60
abbas80 <dehghanzadehabbas@gmail.com>
2025-04-24 14:03:57 +0330

    PATCH: amend CourseSchema, and add Course Interface

--- e-learning[api]/src/content/schema/comments.schema.ts
+++ e-learning[api]/src/content/schema/comments.schema.ts
@@ -1,10 +1,20 @@
 import { Prop, Schema } from "@nestjs/mongoose";
 
-@Schema({ _id: false , timestamps: true})
+@Schema()
 export class Comment {
+  @Prop()
+  id: string;
+
   @Prop({length: 250})
   text: string;
 
   @Prop({isInteger: true})
   rate: number;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
 }
