e5f714091978d569e46e085fed4fd8ccb80ffbe6
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-06 22:37:33 +0330

    implement app-base test (INestApplication), to use VokitileDB.

--- e-learning[api]/tests/auth/routes.e2e-spec.ts
+++ e-learning[api]/tests/auth/routes.e2e-spec.ts
@@ -1,45 +1,72 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../../src/app.module';
 import * as request from 'supertest';
 
-const BASE_URL = 'http://localhost:3024/';
+describe('Auth module', () => {
+  const BASE_URL = 'http://localhost:3024/';
+  let app: INestApplication;
 
-describe('Auth routes', () => {
-  const api = request(BASE_URL);
-
-  describe('POST /api/auth/signup', () => {
-    test('OK response', async () => {
-      const payload = {
-        firstname: 'test',
-        lastname: 'test',
-        username: 'test',
-        phone: '0123456789',
-        email: 'test@test.com',
-        password: 'test1234',
-      };
-      const resp = await api.post('api/auth/signup').send(payload);
-      expect(resp.status).toBe(201);
-      expect(resp.body).toBeDefined();
-      expect(resp.body).toContain('token');
-    });
+  beforeAll(async () => {
+    process.env.environment = 'test';
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  describe('POST /api/auth/login', () => {
-    test('OK response', async () => {
-      const payload = {
-        username: 'test',
-        password: 'test1234',
-      };
-      const resp = await api.post('api/auth/login').send(payload);
-      expect(resp.status).toBe(201);
-      expect(resp.body).toBeDefined();
-      expect(resp.body).toContain('token');
-    });
+  afterAll(async () => {
+    await app.close();
+    process.env.environment = 'dev';
   });
 
-  // describe('/api/auth/logout', async () => {
-  //   const req = await api.delete('/api/auth/login');
-  //   req.headers['Authorization'] = 'Token ABCD';
-  //   expect(resp.status).toBe(201);
-  //   expect(resp.body).toBeDefined();
-  //   expect(resp.body).toContain('token');
-  // });
+  describe('Auth routes', () => {
+    const api = request(BASE_URL);
+
+    describe('POST /api/auth/signup', () => {
+      test('OK response', async () => {
+        const payload = {
+          firstname: 'test',
+          lastname: 'test',
+          username: 'test',
+          phone: '0123456789',
+          email: 'test@test.com',
+          password: 'test1234',
+        };
+        const resp = await request(app.getHttpServer())
+          .post('/api/auth/signup')
+          .send(payload);
+        expect(resp.status).toBe(201);
+        expect(resp.body).toBeDefined();
+        expect(resp.body['access_token']).toBeTruthy();
+      });
+    });
+
+    describe('POST /api/auth/login', () => {
+      test('OK response', async () => {
+        const payload = {
+          username: 'test',
+          password: 'test1234',
+        };
+        const resp = await request(app.getHttpServer())
+          .post('/api/auth/login')
+          .send(payload);
+        expect(resp.status).toBe(201);
+        expect(resp.body).toBeDefined();
+        expect(resp.body['access_token']).toBeTruthy();
+      });
+    });
+
+    describe('/api/auth/logout', () => {
+      test('UnAuthorized response', async () => {
+        const resp = await request(app.getHttpServer())
+          .delete('/api/auth/logout')
+          .send();
+        expect(resp.status).toBe(401);
+        expect(resp.body).toBeDefined();
+        expect(resp.body).toBeTruthy();
+      });
+    });
+  });
 });
