a80dcb6cbcf0458afe4ea78db685cd8728ef0c22
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-14 14:40:12 +0330

    insert cart.entities into CartService.

--- e-learning[api]/src/cart/cart.service.ts
+++ e-learning[api]/src/cart/cart.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cart-item.entity';
 
 @Injectable()
 export class CartService {
+  constructor(
+    @InjectRepository(Cart) private carts: Repository<Cart>,
+    @InjectRepository(CartItem) private cartitems: Repository<CartItem>,
+  ) {}
+
   getCart(id: number) {
     return `Cart ${id}`;
   }
