3f946bfab91117dbf0d3aa5ce2b4055802f54eb6
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-14 14:02:59 +0330

    Write Boilerplate for manipulating Cart `recieve` `create` `discount` `destroy`.

--- e-learning[api]/src/cart/cart.controller.ts
+++ e-learning[api]/src/cart/cart.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CartService } from './cart.service';
 
-@Controller()
+@Controller('api/cart')
 export class CartController {
   constructor(private cartService: CartService) {}
+
+  @Get(':id')
+  recieveCart(@Param('id') id: number) {
+    return this.cartService.getCart(id);
+  }
+  @Post('')
+  createCart() {
+    return this.cartService.createCart();
+  }
+  @Post(':id/discount')
+  discountCart(@Param('id') id: number) {
+    return this.cartService.discountCart(id);
+  }
+  @Delete('id')
+  destroyCart(@Param('id') id: number) {
+    return this.cartService.destroyCart(id);
+  }
 }
--- e-learning[api]/src/cart/cart.service.ts
+++ e-learning[api]/src/cart/cart.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  getCart(id: number) {
+    return `Cart ${id}`;
+  }
+  createCart() {
+    return 'Cart Creation!';
+  }
+  discountCart(id: number) {
+    return `Cart ${id} Off!!!`;
+  }
+  destroyCart(id: number) {
+    return `Cart ${id}`;
+  }
+}
