6997be0f5dac7edaeb9e5afa1caac16afea0f296
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-18 18:42:19 +0330

    use swagger for hello-world route!

--- e-learning[api]/src/app.controller.ts
+++ e-learning[api]/src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({ example: 'Hello World!' })
+  @ApiTags("hello world")
   @Get()
   getHello(): string {
     return this.appService.getHello();
