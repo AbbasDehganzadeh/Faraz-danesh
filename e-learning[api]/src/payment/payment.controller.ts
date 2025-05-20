81229f9908149c78e22ea6bb982736619ab2a3e7
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-20 11:42:27 +0330

    Add routes for paymentController: makepayment, getpayment, purchasepayment, recievepayment, discountpayment

--- e-learning[api]/src/payment/payment.controller.ts
+++ e-learning[api]/src/payment/payment.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 
-@Controller()
+@Controller('payment')
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
+
+  @Post()
+  makePayment() {
+    return this.paymentService.makePayment();
+  }
+
+  @Get(':id')
+  getPayment(@Param('id') id: number) {
+    return this.paymentService.getPayment(id);
+  }
+
+  @Post(':id/purchase')
+  purchasePayment(@Param('id') id: number) {
+    return this.paymentService.purchasePayment(id);
+  }
+
+  @Post(':id/discount')
+  discountPayment(@Param('id') id: number) {
+    return this.paymentService.discountPayment(id);
+  }
+
+  @Get(':id/recieve')
+  recievePayment(@Param('id') id: number) {
+    return this.paymentService.recievePayment(id);
+  }
 }
--- e-learning[api]/src/payment/payment.service.ts
+++ e-learning[api]/src/payment/payment.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  makePayment() {
+    return 'Payment created!';
+  }
+
+  getPayment(id: number) {
+    return `Payment ${id}!`;
+  }
+
+  purchasePayment(id: number) {
+    return `Payment ${id} purchased!`;
+  }
+
+  discountPayment(id: number) {
+    return `Payment ${id} Off!!`;
+  }
+
+  recievePayment(id: number) {
+    return `Payment ${id} recieved!`;
+  }
+}
