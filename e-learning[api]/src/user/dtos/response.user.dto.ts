ef5f77e86000d33bc07a1cee76479f3dfc29d3c8
abbas80 <dehghanzadehabbas@gmail.com>
2025-05-22 15:14:24 +0330

    FixUp ResponseUserDto in order to response properly

--- e-learning[api]/src/user/dtos/response.user.dto.ts
+++ e-learning[api]/src/user/dtos/response.user.dto.ts
@@ -3,32 +3,47 @@ import { roles } from '../../common/enum/roles.enum';
 import { CreateUserDto } from './user.dto';
 
 export class ResponseUserDto extends CreateUserDto {
+  id: number;
+
   @Expose({ name: 'firstname' })
-  @Transform(({ value }) => {
-    if (value !== null || value !== undefined) {
-      return '';
-    }
-    return value;
+  @Transform(({ obj }) => {
+    return obj['fname'] ? obj['fname'] : '';
   })
   fname: string;
   @Expose({ name: 'lastname' })
-  @Transform(({ value }) => {
-    if (value !== null || value !== undefined) {
-      return '';
-    }
-    return value;
+  @Transform(({ obj }) => {
+    return obj['lname'] ? obj['lname'] : '';
   })
   lname: string;
+
   @Expose({ name: 'username' })
+  @Transform(({ obj }) => {
+    return obj['uname'] ? obj['uname'] : '';
+  })
   uname: string;
+
   @Transform(({ value }) => {
-    if (value !== null || value !== undefined) {
-      return '';
-    }
-    return value;
+    return value ? value : '';
+  })
+  email: string;
+
+  @Transform(({ value }) => {
+    return value ? value : '';
   })
   phone: string;
+
   @Exclude()
   password: string;
+
+  @Transform(({ value }) => {
+    switch (value) {
+      case 1:
+        return 'Teacher';
+      case 2:
+        return 'Supervisor';
+      default:
+        return 'Student';
+    }
+  })
   role: roles;
 }
